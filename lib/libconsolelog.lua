2787aab94055ce50dbec907fd649e572f588621e
akuankka128 <30865373+akuankka128@users.noreply.github.com>
2019-09-23 19:10:06 +0300

    Added comments

--- lib/libconsolelog.lua
+++ lib/libconsolelog.lua
@@ -1,4 +1,4 @@
-_G.console = {};
-function console.log(argv)
-    print(argv);
-end
+_G.console = {}; -- Declaring the global table console
+function console.log(args) -- Creates the console.log function with the arguments args
+    print(args); -- Prints args
+end -- End
