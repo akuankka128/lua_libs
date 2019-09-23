6e505a9d8a99d720ddf7f26501b3908ff6d0eb86
akuankka128 <30865373+akuankka128@users.noreply.github.com>
2019-09-23 19:15:27 +0300

    Added comments and the lib name tag

--- lib/oldindex.lua
+++ lib/oldindex.lua
@@ -1,12 +1,14 @@
-function index(tab, startIndex)
-	i = 0
-	for _,v in pairs(tab) do
-		if startIndex == 1 then
-			i = i + 1
-			tab[i] = v
-		else
-			tab[i] = v
-			i = i + 1
-		end
-	end
-end
+--[[-- index from indexfunctions.lua but the older version --]]--
+
+function index(tab, startIndex) -- Declares the function
+	i = 0 -- Declares a global variable named i to value 0
+	for _,v in pairs(tab) do -- Loop through all key-value pairs of table tab
+		if startIndex == 1 then -- Checks if startIndex is 1
+			i = i + 1 -- Increments i by 1
+			tab[i] = v -- Sets table's index i to value v
+		else -- Else
+			tab[i] = v -- Sets table's index i to value v
+			i = i + 1 -- Increments i by 1
+		end -- End
+	end -- End
+end -- End
