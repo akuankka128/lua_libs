f267c5e23b9efb0edc5cfce3c0a4968b1fbdd212
akuankka128 <30865373+akuankka128@users.noreply.github.com>
2019-09-23 19:01:47 +0300

    Added comments and an example

--- lib/importloader.lua
+++ lib/importloader.lua
@@ -1,9 +1,15 @@
 --[[-- Import Loader v1.1 --]]--
 
-function import( imports )
-    assert(type( imports ) == "table", "table expected, got "..type( imports ));
-    for _,f in ipairs( imports ) do
-        assert(type(f) == "string", "string expected, got "..type(f));
-        dofile(f);
-    end
-end
+function import( imports ) -- Declaring the function
+    assert(type( imports ) == "table", "table expected, got "..type( imports )); -- Checking if argument is a table, if not, throw an error
+    for _,f in ipairs( imports ) do -- Loop through all table keys and values
+        assert(type(f) == "string", "string expected, got "..type(f)); -- Checking if current table item is a string, otherwise throw an error
+        dofile(f); -- Try loading the file in the argument
+    end -- Obvious
+end -- Obvious 2: revisited
+
+--[[
+    Example usage:
+    import({"./lib/sleep.lua","./lib/libconsolelog.lua"});
+    ^ Imports those files, in order
+]]
