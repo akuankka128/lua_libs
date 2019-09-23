f37fb332f4249bc09430e3095c90a4f05a2703c5
akuankka128 <30865373+akuankka128@users.noreply.github.com>
2019-09-23 19:17:07 +0300

    Added comments and lib name tag

--- lib/sleep.lua
+++ lib/sleep.lua
@@ -1,6 +1,10 @@
-local clock = os.clock
-function sleep(t)  -- seconds
-	local t0 = clock()
-	while clock() - t0 <= t do
-	end
-end
+--[[-- Sleep lib v1.0 --]]--
+
+local clock = os.clock -- Declares the local variable clock
+function sleep(t)  -- Declares the function
+	local t0 = clock() -- idk
+	while clock() - t0 <= t do -- idk 2
+	end -- End
+end -- End
+
+--[[ Copypasted this cuz I was too lazy to figure it out myself :p ]]--
