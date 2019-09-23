0d1b5e9e4f9bba4aa07c7bfa87e19c3a4478f467
akuankka128 <30865373+akuankka128@users.noreply.github.com>
2019-09-23 19:12:46 +0300

    Added comments

--- lib/libtablefunctions.lua
+++ lib/libtablefunctions.lua
@@ -4,19 +4,19 @@
     - WONTFIX returns "table (address)"
 ]]--
 
-function val(tab)
-    local temp = {};
-    assert(type(tab) == "table", "table expected, got "..type(tab));
-    for _,v in pairs(tab) do
-        _G.table.insert(temp, v);
-    end
-    return temp;
-end
-function key(tab)
-    local temp = {};
-    assert(type(tab) == "table", "table expected, got "..type(tab));
-    for k, _ in pairs(tab) do
-        _G.table.insert(temp, k);
-    end
-    return temp;
-end
+function val(tab) -- Declares a function named val
+    local temp = {}; -- Declares a local table named temp
+    assert(type(tab) == "table", "table expected, got "..type(tab)); -- Checks if tab is a table, else throw an error
+    for _,v in pairs(tab) do -- Loops through all values
+        _G.table.insert(temp, v); -- Inserts value v to table temp
+    end -- End
+    return temp; -- Returns the temp table
+end -- End
+function key(tab) -- Declares a function named key
+    local temp = {}; -- Declares a local table named temp
+    assert(type(tab) == "table", "table expected, got "..type(tab)); -- Checks if tab is a table, else throw an error
+    for k, _ in pairs(tab) do -- Loops through all keys
+        _G.table.insert(temp, k); -- Inserts value k to table temp
+    end -- End
+    return temp; -- Returns the temp table
+end -- End
