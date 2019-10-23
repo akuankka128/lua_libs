35388b31ac01d36a5de4f7a8b971e186dd6f419a
akuankka128 <30865373+akuankka128@users.noreply.github.com>
2019-10-23 18:01:24 +0300

    Update libconsolelog.lua

--- lib/libconsolelog.lua
+++ lib/libconsolelog.lua
@@ -1,4 +1,4 @@
-_G.console = {}; -- Declaring the global table console
-function console.log(args) -- Creates the console.log function with the arguments args
-    print(args); -- Prints args
-end -- End
+_G.console = {};
+function console:log( args )
+    print( args );
+end
