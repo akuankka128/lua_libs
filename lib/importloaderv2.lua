b67ecdf33301efb8a8944fe1f6f588c7b4497383
akuankka128 <30865373+akuankka128@users.noreply.github.com>
2020-03-15 20:48:01 +0200

    Update importloaderv2.lua

--- lib/importloaderv2.lua
+++ lib/importloaderv2.lua
@@ -1,18 +1,25 @@
 --[[-- Import Loader v2 --]]--
 
-
-function import( imports ) -- Declaring the function
+--- Main function
+-- @param {string | table<string>} imports Table of strings or string of files to be imported.
+function import( imports )
     assert(type( imports ) == "table" or type( imports ) == "string", "table or string expected, got "..type( imports )); -- Checking if argument is a table or a string, if not, throw an error
     if type( imports ) == "table" then
 	    for _,f in ipairs( imports ) do -- Loop through all table keys and values
 	        assert(type(f) == "string", "string expected, got "..type(f)); -- Checking if current table item is a string, otherwise throw an error
-	        dofile(f); -- Try loading the file in the argument
-	    end -- Obvious
-	end -- Obious 2: revisited
+	        print("Importing file "..f.."..."); -- Relevant information
+	        if pcall(function() dofile(f); end) then do -- pcall dofile(...)
+	        	-- No errors
+	        	-- print("Successfully imported file "..f);
+	        end else do
+	        	print("Errors found while importing file "..f.."."); -- Let's not provide relevant information
+	    	end end
+	    end
+	end
 	if type( imports ) == "string" then
-		dofile( imports ); -- I'm not gonna help you out here, you gotta figure out what this does yourself
-	end -- Obvious 3: the grand final
-end -- Obvious 4: reborn
+		dofile( imports ); -- Load file
+	end
+end
 
 _G.import = import; -- Make it global too! Because why not?
 
